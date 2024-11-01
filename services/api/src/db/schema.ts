b41b71b667479b298ce7104fc28a0fb137d7e62e
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-11-01 14:35:02 +0000

    feat: create exercise table

--- services/api/src/db/schema.ts
+++ services/api/src/db/schema.ts
@@ -1,7 +1,13 @@
-import { pgTable, pgSchema, serial, text, varchar, uuid } from "drizzle-orm/pg-core";
+import {pgTable, pgSchema, varchar, uuid, integer} from "drizzle-orm/pg-core";
 
 const authSchema = pgSchema('auth');
 
 const users = authSchema.table('users', {
 	id: uuid('id').primaryKey(),
 });
+
+export const exercises = pgTable('exercises', {
+	id: integer().primaryKey().generatedByDefaultAsIdentity(),
+	user_id: uuid().references(() => users.id).notNull(),
+	name: varchar({ length: 255 }).notNull(),
+})
