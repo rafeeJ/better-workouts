9cd0079161460ee071318c6eaed4fe9b4d6ba841
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-11-12 23:45:45 +0000

    feat: auth

--- services/web/app/exercises/page.tsx
+++ services/web/app/exercises/page.tsx
@@ -1,8 +1,20 @@
 import { Exercises } from "@/components/exercises/exercises"
 import { RecentExercises } from "@/components/exercises/recent-exercises"
+import { createClient } from "@/utils/supabase/server";
 import { Suspense } from "react"
+import { redirect } from "next/navigation";
+  
+export default async function ExercisesPage() {
+  const supabase = await createClient();
 
-export default function ExercisesPage() {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
   return (
     <div className="container py-8">
       <div className="flex flex-col gap-4">
--- services/web/app/workouts/page.tsx
+++ services/web/app/workouts/page.tsx
@@ -1,6 +1,18 @@
 import WorkoutCalendar from "@/components/workouts/workout-calendar";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function WorkoutsPage() {
+export default async function WorkoutsPage() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
   return (
     <div className="container py-8">
       <div className="flex justify-between items-center mb-8">
